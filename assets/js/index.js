a5b3385f636c912ad0700cad203c8c8ab8c73970
Esther Vianna <esther.avianna@gmail.com>
2023-03-17 08:50:08 -0300

    add js functions e ajustes mediaqueries

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,14 @@
 function showInput() {
+  if (window.innerWidth < 720){
     const input = document.querySelector("#loc");
     input.style.visibility = "visible";
+  }
 }
 
 
   function hideInput() {
+    if (window.innerWidth < 720){
     const input = document.querySelector("#loc");
     input.style.visibility = "hidden";
+    }
   }
