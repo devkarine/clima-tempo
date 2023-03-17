37260a54fee38ef7d5ade0ff8d942a050d6fc741
Esther Vianna <esther.avianna@gmail.com>
2023-03-16 21:32:44 -0300

    ajustes mediaQueries

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+function showInput() {
+    const input = document.querySelector("#loc");
+    input.style.visibility = "visible";
+}
+
+
+  function hideInput() {
+    const input = document.querySelector("#loc");
+    input.style.visibility = "hidden";
+  }
